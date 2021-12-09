2f3d9c725a9a9ec6f28eb02556a255387e4810fa
nikblaze <kumarnikhil237@gmail.com>
2021-12-09 10:38:21 +0530

    DB connected

--- controller/task.js
+++ controller/task.js
@@ -1,18 +1,27 @@
+const Task= require('../models/Task')
+
 const getAllTask=(req,res)=>{
   res.send('all item for getAllTask')
 }
-const createTask=(req,res)=>{
-  res.json(req.body)
+
+
+const createTask= async (req,res)=>{
+  const task = await Task.create(req.body)
+  res.status(201).json({task})
 }
+
 const getTask=(req,res)=>{
   res.json({id:req.params.id})
 }
+
 const updateTask=(req,res)=>{
   res.send('all item for updateTask')
 }
+
 const deleteTask=(req,res)=>{
   res.send('all item for deleteTask')
 }
+
 module.exports={
   getAllTask,
   createTask,
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-
 const express=require('express');
 const app=express();
 const task=require('./routes/task');
+
+const connectDB = require('./db/connect')
+require('dotenv').config()
 // middleware
 app.use(express.json())
 app.use('/task',task)
 
-app.listen(3000,()=>{
-  console.log(" server is up and listening .. ");
+const start = async () =>{
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(3000,()=>{
+      console.log("Server is listening .. ");
+    })
 
-})
+  } catch (e) {
+    console.log(e);
+  }
+}
+start()
